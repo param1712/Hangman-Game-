a6483242d6093a85758bdf0d658222fba8f76f4e
param1712 <109943976+param1712@users.noreply.github.com>
2022-08-04 23:39:24 +0530

    Origin

---
+++ Python/hangman.py
@@ -0,0 +1,45 @@
+import random
+from words import words
+import string
+
+def validWord(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word
+
+def hangman():
+    word = validWord(words).upper()
+    wordLetters = set(word) #the letters of chosen word
+    alphabet= set(string.ascii_uppercase)
+    usedLetters= set() #what user has guessed 
+    lives = 7
+
+    while len(wordLetters) > 0 and lives > 0:
+        
+        #letters used
+        #''.join(['a','b' ,'c']) means string printed as 'a b c' 
+        print('You have guessed: ', ' '.join(usedLetters))
+
+        #tell current word with dash
+        wordList=[letter if letter in usedLetters else '-' for letter in word]
+        print("lives remaining",lives, 'Current word: ', ' '.join(wordList))
+        userInput= input('Guess a letter: ').upper()  #getting input
+        if userInput in alphabet not in usedLetters:
+            usedLetters.add(userInput)
+            if userInput in wordLetters:
+                wordLetters.remove(userInput)
+            else:
+                lives= lives-1
+                print("letter not in the word")
+
+        elif userInput in userInput:
+            print ('you have already guessed that letter')
+
+        else: print ('Invalid Guess. try again')
+    if lives == 0:
+        print("Sorry you lost, The correct word is ", word)
+    else:
+        print("congrats you have guessed the correct word ", word) #gets here when guessed all words
+
+hangman()
